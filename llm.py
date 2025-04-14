86ef1e594858846e803d45947e0564a03fb334d3
minzunim <greenteapig6@gmail.com>
2025-04-15 07:39:55 +0900

    [FEAT] chunk 단위로 5페이지까지 스크래핑

--- llm.py
+++ llm.py
@@ -2,15 +2,20 @@ import os
 from dotenv import load_dotenv
 import requests
 
+import time
+import random 
+
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
 
+# 모델 토큰 제한
+MAX_TOKENS = 8192
+CHUNK_SIZE = 2000  # 보낼 때 여유를 두기 위한 크기
+
 def extract_keywords(text: str, count: int = 5) -> list[str]:
-    prompt = f'''다음 텍스트는 미국 주식 관련 커뮤니티에 올라온 글의 제목과 본문이야.\n
-                텍스트를 분석해서 지금 사람들이 미국 주식과 관련해서 무엇을 관심있게 얘기하는지 알고 싶어.\n 
-                주요 키워드 {count}개만 뽑아줘:\n\n{text}'''
+    prompt = f'''다음 텍스트를 분석해서 미국 주식 현황을 2~3문장으로 요약해줘:\n{text}'''
 
     headers = {
         "Authorization": f"Bearer {GROQ_API_KEY}",
@@ -24,26 +29,32 @@ def extract_keywords(text: str, count: int = 5) -> list[str]:
         ]
     }
 
+    chunks = [text[i:i+CHUNK_SIZE] for i in range(0, len(text), CHUNK_SIZE)]
+    results = []
+    for chunk in chunks:
+        data["messages"][0]["content"] = chunk
+        response = requests.post(GROQ_API_URL, headers=headers, json=data)
+        response.raise_for_status()  # 요청 실패 시 예외 발생
+        print(response.json())
+        results.append(response.json()["choices"][0]["message"]["content"])
+        time.sleep(random.uniform(1, 2))
+    combined_text = " ".join(results)
+
+    # response = requests.post(GROQ_API_URL, headers=headers, json=data)
+    # response.raise_for_status()
+    # result = response.json()
+
+    # return result["choices"][0]["message"]["content"].strip().split("\n"
+
+    # 최종 요약 요청
+    final_prompt = f'''지금까지 요약한 텍스트를 마지막으로 한번 더 요약하고 한국어로 번역해서 2~3문장으로 말해줘:\n{combined_text}'''
+    data["messages"][0]["content"] = final_prompt
+
+    # 요약 요청
+    time.sleep(random.uniform(2, 3))
+
     response = requests.post(GROQ_API_URL, headers=headers, json=data)
-    response.raise_for_status()
-    result = response.json()
+    response.raise_for_status()  # 요청 실패 시 예외 발생
+    final_summary = response.json()["choices"][0]["message"]["content"]
 
-    return result["choices"][0]["message"]["content"].strip().split("\n")
-
-'''{
-keywords: [
-"Here are 5 major keywords related to stock market and trading that I extracted from the text:",
-"",
-"1. **NASDAQ** (나스닥) - mentioned multiple times in the text, indicating that the author is interested in the performance of the NASDAQ stock market.",
-"2. **-Trump** (트럼프) - mentioned several times, suggesting that the author is interested in the impact of Trump's policies and actions on the stock market.",
-"3. **Long/Short** (롱/숏) - mentioned frequently, indicating that the author is engaged in trading activities and is concerned with long-term investments vs. short-term gains/losses.",
-"4. **Tariffs** (관세) - mentioned several times, indicating that the author is interested in the impact of trade tariffs on the stock market and economy.",
-"5. **AAPL** (애플) - mentioned once, indicating that the author is interested in the performance of Apple Inc.'s stock.",
-"",
-"These keywords suggest that the author is an active trader or investor interested in the US stock market, particularly in the tech sector, and is paying close attention to geopolitical events and their impact on the market."
-]
-}
-'''
-
-# 프롬프트가 중요하겠다. 단순히 키워드 뽑아내는 것만이 중요할까...?
-# 그리고 8000자 이 정도도 그런데 그 이상을 음..
+    return final_summary
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# main.py
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import Union
 
 from fastapi import FastAPI
@@ -18,6 +21,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 import time
 
 from llm import extract_keywords
+import random
 
 
 # 모델 설정
@@ -27,8 +31,6 @@ class Item(BaseModel):
     is_offer: Union[bool, None] = None
 
 app = FastAPI()
-
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -76,72 +78,193 @@ def get_market_summary():
     return {"data": total_list}
 
 # 구글 스프레드 시트에 글 정보 저장
-@app.get("/post_scrap")
-def post_scrap():
+@app.get("/scrap_posts")
+def scrap_posts():
 
+    # user_agents 목록
+    USER_AGENTS = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/118.0.5993.70 Safari/537.36",
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Mobile/15E148 Safari/604.1",
+        "Mozilla/5.0 (iPad; CPU OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Mobile/15E148 Safari/604.1",
+    ]
+
+    # user_agent 랜덤 선택
     headers = {
-        "User-Agent": "Mozilla/5.0",
+        "User-Agent": random.choice(USER_AGENTS)
     }
 
-    url = f"https://gall.dcinside.com/mgallery/board/lists/?id=stockus&page=1" # 페이징으로 호출
-    res = requests.get(url, headers=headers)
+    # 기존 스크래핑 데이터 열기
+    sheet = client.open("stockus-posts").sheet1.get_all_records() # 객체 형태로 반환
+    max_id = max(sheet, key=lambda x: x["id"]) # 현재까지 저장된 가장 큰 아이디 값
+    
+    while True:
+        page_count = 1
 
-    # 1페이지에 있는 전체 글 번호, 제목, 링크 수집
-    if res.status_code == 200:
-        # request 결과 파싱해서 soup에 저장
-        soup = BeautifulSoup(res.text, "html.parser")
-        
-        posts = []
+        url = f"https://gall.dcinside.com/mgallery/board/lists/?id=stockus&page={page_count}" # 페이징으로 호출
+        res = requests.get(url, headers=headers)
 
-        for row in soup.find_all("tr", class_="ub-content")[9:]:
+        # 1페이지에 있는 전체 글 번호, 제목, 링크 수집
+        if res.status_code == 200:
+            soup = BeautifulSoup(res.text, "html.parser")
+            contents_list = soup.find_all(
+                lambda tag: (
+                    tag.name == "tr"
+                    and "us-post" in tag.get("class", [])
+                    and tag.get("data-type") != "icon_notice" # 공지 설문 제외
+                )
+            )
 
-            post_id = row.select_one("td.gall_num").text.strip()
+            posts = []
 
-            title_tag = row.select_one("td.gall_tit a")
-            date = row.select_one("td.gall_date").get("title")
-            views = row.select_one("td.gall_count").text.strip()
-            recommend = row.select_one("td.gall_recommend").text.strip()
-            second_div = ""
+            for i, row in enumerate(contents_list):
+                print(i, row)
 
-            url = f"https://gall.dcinside.com/mgallery/board/view/?id=stockus&no={post_id}" # 페이징으로 호출
-            res = requests.get(url, headers=headers)
-
-            if res.status_code == 200:
-                soup = BeautifulSoup(res.text, "html.parser")
-
-                contents = soup.find("div", class_="write_div")
-                if contents == None:
+                post_id = row.select_one("td.gall_num").text.strip()
+                if post_id == max_id:
                     continue
-                else:
-                    divs = contents.find_all(["p", "span", "div"])
-                    if len(divs) > 0:
-                        second_div = " ".join(list(set([div.text for div in divs])))
-                    time.sleep(1.5)
 
-            # if not title_tag:
-            #     continue  # 제목 없는 건 스킵
+                title_tag = row.select_one("td.gall_tit a")
+                date = row.select_one("td.gall_date").get("title")
+                views = row.select_one("td.gall_count").text.strip()
+                recommend = row.select_one("td.gall_recommend").text.strip()
+                second_div = ""
 
-            post = {
-                "id": post_id,
-                "title": title_tag.text.strip(),
-                "date": date,
-                "views": views,
-                "recommend": recommend,
-                "contents": second_div
-            }
+                url = f"https://gall.dcinside.com/mgallery/board/view/?id=stockus&no={post_id}" # 개별 게시글 호출
+                res = requests.get(url, headers=headers)
 
-            post_list = list(post.values())
+                if res.status_code == 200:
+                    soup = BeautifulSoup(res.text, "html.parser")
 
-            posts.append(post_list)
-    else:
-        print(res)
+                    contents = soup.find("div", class_="write_div")
+                    if contents == None:
+                        continue
+                    else:
+                        divs = contents.find_all(["p", "span", "div"])
+                        if len(divs) > 0:
+                            second_div = " ".join(list(set([div.text for div in divs])))
+                        time.sleep(1.5)
+
+                post = {
+                    "id": post_id,
+                    "title": title_tag.text.strip(),
+                    "date": date,
+                    "views": views,
+                    "recommend": recommend,
+                    "contents": second_div
+                }
+
+                post_list = list(post.values())
+
+                posts.append(post_list)
+                # 마지막 게시글일 때
+                if len(contents_list) - 1 == i:
+                    page_count += 1 
+        else:
+            print(res)
+            return
+
+        sheet = client.open("stockus-posts").sheet1
+        sheet.append_rows(posts)
+
         return
 
-    sheet = client.open("미주마갤 스크래핑").sheet1
-    sheet.append_rows(posts)
+# 멀티 스레딩 테스트용  
+@app.get("/scrap_posts_multi")
+def scrap_posts():
+    USER_AGENTS = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/118.0.5993.70 Safari/537.36",
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Mobile/15E148 Safari/604.1",
+        "Mozilla/5.0 (iPad; CPU OS 15_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Mobile/15E148 Safari/604.1",
+    ]
 
-    return
+    headers = {"User-Agent": random.choice(USER_AGENTS)}
+    sheet = client.open("stockus-posts").sheet1.get_all_records()
+    #max_id = max(sheet, key=lambda x: x["id"])["id"]
 
+    # 본문 스크래핑
+    def fetch_post_data(post_id):
+        url = f"https://gall.dcinside.com/mgallery/board/view/?id=stockus&no={post_id}"
+        res = requests.get(url, headers=headers)
+        time.sleep(random.uniform(0, 1)) # 0~1초 랜덤
+        
+        if res.status_code == 200:
+            soup = BeautifulSoup(res.text, "html.parser")
+            contents = soup.find("div", class_="write_div")
+            if contents:
+                divs = contents.find_all(["p", "span", "div"])
+                return " ".join(list(set([div.text for div in divs])))
+        return ""
+    
+    # 페이지별 목록 스크래핑 
+    def fetch_page_data(page_count):
+        url = f"https://gall.dcinside.com/mgallery/board/lists/?id=stockus&page={page_count}"
+        res = requests.get(url, headers=headers)
+        time.sleep(random.uniform(1, 2))  # 1~2초 랜덤 딜레이
+
+        if res.status_code == 200:
+            soup = BeautifulSoup(res.text, "html.parser")
+            contents_list = soup.find_all(
+                lambda tag: (
+                    tag.name == "tr"
+                    and "us-post" in tag.get("class", [])
+                    and tag.get("data-type") != "icon_notice"
+                )
+            )
+            posts = []
+            for row in contents_list:
+                post_id = row.select_one("td.gall_num").text.strip()
+                # if post_id == max_id:
+                #     continue
+                title_tag = row.select_one("td.gall_tit a")
+                date = row.select_one("td.gall_date").get("title")
+                views = row.select_one("td.gall_count").text.strip()
+                recommend = row.select_one("td.gall_recommend").text.strip()
+                contents = fetch_post_data(post_id)
+                post = {
+                    "id": post_id,
+                    "title": title_tag.text.strip(),
+                    "date": date,
+                    "views": views,
+                    "recommend": recommend,
+                    "contents": contents.replace("- dc official App", "").strip(),
+                }
+                print(post)
+                posts.append(post)
+            return posts
+        return []
+
+    posts = []
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        futures = [executor.submit(fetch_page_data, page) for page in range(1, 6)]  # Adjust range as needed
+        for future in as_completed(futures):
+            posts.extend(future.result())
+
+    sheet = client.open("stockus-posts").sheet1
+    sheet.append_rows([list(post.values()) for post in posts])
+
+    return {"status": "success", "posts_count": len(posts)}
+
+
+# 키워드 추출
+@app.get("/extract_keywords")
+async def get_keywords():
+    # 스프레드 시트에서 가져오기
+    sheet = client.open("stockus-posts").sheet1
+    
+    title_data = sheet.col_values(2)[1:]
+    contents_data = sheet.col_values(6)[1:]
+
+    concat_list = [val for pair in zip(title_data, contents_data) for val in pair if val != '' or val != '- dc official App']   
+    full_text = " ".join(concat_list).replace("- dc official App","")
+
+    result = extract_keywords(full_text)
+    return {"keywords": result}
+
+# 테스트용
 @app.get("/test")
 def read_item():      
     html = '''<div class="write_div" style="overflow:hidden;width:900px;" data-tracking="feb09862036defa1cb39229b3c4b4107c46f6805a6653be982e20534f83c45">
@@ -153,20 +276,3 @@ def read_item():
     print(list)
 
     return
-
-# 키워드 추출
-@app.get("/extract_keywords")
-async def get_keywords():
-    # 스프레드 시트에서 가져오기
-    sheet = client.open("미주마갤 스크래핑").sheet1
-    
-    title_data = sheet.col_values(2)[1:]
-    contents_data = sheet.col_values(6)[1:]
-
-    concat_list = [val for pair in zip(title_data, contents_data) for val in pair if val != '' or val != '- dc official App']
-    full_text = " ".join(concat_list).replace("- dc official App","")
-    
-    #print(len(full_text.replace(" ", "")))
-
-    result = extract_keywords(full_text)
-    return {"keywords": result}
