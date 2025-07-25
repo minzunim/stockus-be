3ce5e0ceae440a1f5ff1f78cdb058516a00c9dbd
minzunim <greenteapig6@gmail.com>
2025-07-26 08:54:52 +0900

    [Fix] 배포 환경에서 supbase_url is reqitred 오류 해결 위한 테스트

--- supabase_client.py
+++ supabase_client.py
@@ -1,3 +1,4 @@
+'''
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -5,5 +6,20 @@ load_dotenv()
 from supabase import create_client, Client
 
 url: str = os.getenv("SUPABASE_URL")
+print('url', url)
 key: str = os.getenv("SUPABASE_KEY")
+print('key', key)
+
+supabase: Client = create_client(url, key)
+'''
+
+import os
+from supabase import create_client, Client
+
+url: str = os.environ.get("SUPABASE_URL")
+print('url', url)
+
+key: str = os.environ.get("SUPABASE_KEY")
+print('key', key)
+
 supabase: Client = create_client(url, key)
