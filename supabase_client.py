f44e3c9669c653689a74210df58db67041134801
Minju Hwang <greenteapig6@gmail.com>
2025-07-28 12:07:47 +0900

    [Fix] 크론 테스트 시간 원복, supabse 환경 변수 연결 방식 수정

--- supabase_client.py
+++ supabase_client.py
@@ -14,12 +14,20 @@ supabase: Client = create_client(url, key)
 '''
 
 import os
+from dotenv import load_dotenv
 from supabase import create_client, Client
+
+# .env 파일 로드
+load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL")
 print('url', url)
 
 key: str = os.environ.get("SUPABASE_KEY")
 print('key', key)
+
+# 환경 변수가 없으면 에러 발생
+if not url or not key:
+    raise ValueError("SUPABASE_URL과 SUPABASE_KEY 환경 변수를 설정해주세요.")
 
 supabase: Client = create_client(url, key)
